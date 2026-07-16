0631355b700c8b6ff5d4632e9d5d726f2a9b4f14
Taylor Otwell <taylor@laravel.com>
2026-07-15 20:59:59 -0500

    add routes

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-app
+
+go 1.22
+
+require github.com/gin-gonic/gin v1.10.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"cmp"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Go")
+	})
+
+	router.Run(":" + cmp.Or(os.Getenv("PORT"), "3000"))
+}
